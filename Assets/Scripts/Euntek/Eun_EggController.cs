a273ca5e3d726d3a17738cde42a188e1c8dad0d9
CherryRain <117277361+cherryrainOuO@users.noreply.github.com>
2023-08-13 02:01:18 +0900

    폰트랑 해상도 정리

--- Assets/Scripts/Euntek/Eun_EggController.cs
+++ Assets/Scripts/Euntek/Eun_EggController.cs
@@ -6,26 +6,49 @@ public class Eun_EggController : MonoBehaviour
 {
     // [SerializeField] private 
     [SerializeField] private Eun_RandomUnitByGrid randomUnitByGrid;
-    [SerializeField] private Transform[] eggs; // 3개 돌리기
+    [SerializeField] private Kyun_NatureEggUnit[] eggs; // 3개 돌리기
+    private bool isCounting = false;
 
     private void Start()
     {
         Vector2 pos = randomUnitByGrid.EggInit();
 
+        eggs[0].transform.position = pos;
         eggs[0].gameObject.SetActive(true);
-        eggs[0].position = pos;
 
+        CoroutineForEggTimeCounting(eggs[0]);
+    }
+    public void GetEgg(Kyun_NatureEggUnit _egg)
+    {
+        isCounting = false;
+        Vector2 pos = randomUnitByGrid.EggInit();
 
+        _egg.transform.position = pos;
+
+        CoroutineForEggTimeCounting(_egg);
     }
 
-    private IEnumerator CoroutineForEggTimeCount()
+
+    private IEnumerator CoroutineForEggTimeCounting(Kyun_NatureEggUnit _egg)
     {
+        isCounting = true;
+
         float time = 0f;
 
         while (time <= 5f)
         {
             time += Time.deltaTime;
             yield return null;
+        }
+
+        if (isCounting)
+        {
+            isCounting = false;
+            Vector2 pos = randomUnitByGrid.EggInit();
+
+            _egg.transform.position = pos;
+
+            CoroutineForEggTimeCounting(_egg);
         }
     }
 
