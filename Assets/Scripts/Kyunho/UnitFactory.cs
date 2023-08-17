1427c507a2da2e856b00a0fe50744adaf0368c71
Coremos <47705451+Coremos@users.noreply.github.com>
2023-08-18 00:35:40 +0900

    유닛 생성 클래스 추가
    - 유닛을 생성하는 클래스를 별도로 분리
    - 추후 오브젝트 풀링을 이용하는 것도 덧붙일 예정

---
+++ Assets/Scripts/Kyunho/UnitFactory.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class UnitFactory : MonoBehaviour
+{
+    [SerializeField] private SpriteIndicator_Chick chickSpriteIndicatorPrefab;
+    [SerializeField] private SpriteIndicator_Chicken chickenSpriteIndicatorPrefab;
+    [SerializeField] private SpriteIndicator_ChickBone chickBoneSpriteIndicatorPrefab;
+    [SerializeField] private SpriteIndicator_NatureEgg natureEggSpriteIndicatorPrefab;
+    [SerializeField] private SpriteIndicator_Egg eggSpriteIndicatorPrefab;
+    [SerializeField] private SpriteIndicator_Pork porkSpriteIndicatorPrefab;
+
+    public IUnit CreateUnit(UnitType type)
+    {
+        ISpriteIndicator spriteIndicator = CreateSpriteIndicator(type);
+        switch (type)
+        {
+            case UnitType.Chicken:
+                return new Unit_Chicken(spriteIndicator);
+            case UnitType.Chick:
+                return new Unit_Chick(spriteIndicator);
+            case UnitType.ChickBone:
+                return new Unit_ChickBone(spriteIndicator);
+            case UnitType.Egg:
+                return new Unit_Egg(spriteIndicator);
+            case UnitType.NatureEgg:
+                return new Unit_NatureEgg(spriteIndicator);
+            case UnitType.Pork:
+                return new Unit_Pork(spriteIndicator);
+            case UnitType.Boss:
+                break;
+            case UnitType.None:
+                break;
+        }
+        return null;
+    }
+
+    public ISpriteIndicator CreateSpriteIndicator(UnitType type)
+    {
+        switch (type)
+        {
+            case UnitType.Chicken:
+                return Instantiate(chickenSpriteIndicatorPrefab);
+            case UnitType.Chick:
+                return Instantiate(chickSpriteIndicatorPrefab);
+            case UnitType.ChickBone:
+                return Instantiate(chickBoneSpriteIndicatorPrefab);
+            case UnitType.Egg:
+                return Instantiate(eggSpriteIndicatorPrefab);
+            case UnitType.NatureEgg:
+                return Instantiate(natureEggSpriteIndicatorPrefab);
+            case UnitType.Pork:
+                return Instantiate(porkSpriteIndicatorPrefab);
+        }
+        return null;
+    }
+
+}
